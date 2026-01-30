f228dbcb8ed7154389d8f75b8ff8339f416cf4b9
Maksim Vlasov <89015773+marmonkin@users.noreply.github.com>
2026-01-30 22:31:51 +0200

    spider

--- player_guy.gd
+++ player_guy.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 
 func _physics_process(delta: float) -> void:
-	#var direction = Input.get_vector()
-	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
 	move_and_slide()
