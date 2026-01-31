b3064269c6ff8d0199fa7873046ba4b665ca5f4f
Vuorinen Artturi TTK23SPO <artturivuorinen@kamk.fi>
2026-02-01 01:01:02 +0200

    game loop

--- spawnerscript.gd
+++ spawnerscript.gd
@@ -2,26 +2,32 @@ extends Node2D
 
 @export var spawnspot: PackedScene = preload("res://objects/spawnspotter.tscn")
 
-var maxspawnamount := 4
-var spawnamount := 0
 var spawn_positions = [
 	Vector2(40, 120),
 	Vector2(160, 40),
 	Vector2(280, 120),
 	Vector2(160, 200)
 ]
-var spawn_cooldown := 0.0
-var spawn_delay := 0.1
 
-func _process(delta: float) -> void:
-	spawn_cooldown -= delta
+var spawn_index := 0
+var spawning := false
 
-	var enemies_alive = get_tree().get_nodes_in_group("enemy").size()
+func _process(_delta: float) -> void:
+	var enemies_alive := get_tree().get_nodes_in_group("enemy").size()
+	var spawnspots_alive := get_tree().get_nodes_in_group("spawnspot").size()
 
-	if (enemies_alive == 0) and (spawnamount < maxspawnamount) and (spawn_cooldown <= 0.0):
-		var create = spawnspot.instantiate()
-		create.global_position = spawn_positions[spawnamount]
-		get_tree().current_scene.add_child(create)
-		print(spawn_positions[spawnamount])
-		spawnamount += 1
-		spawn_cooldown = spawn_delay
+	# Start wave
+	if enemies_alive == 0 and spawnspots_alive == 0 and not spawning:
+		spawning = true
+		spawn_index = 0
+
+	# Spawn spots ONCE
+	if spawning and spawn_index < spawn_positions.size():
+		var s = spawnspot.instantiate()
+		s.global_position = spawn_positions[spawn_index]
+		get_tree().current_scene.add_child(s)
+		spawn_index += 1
+
+	# End wave when all spots created
+	if spawn_index >= spawn_positions.size():
+		spawning = false
