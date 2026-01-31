7bb14f5f59c559c7d60948bc3b72342ba310cff2
Maksim Vlasov <89015773+marmonkin@users.noreply.github.com>
2026-01-31 22:31:19 +0200

    diddi

--- script/item_spawn.gd
+++ script/item_spawn.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var items : Array [Resource]
 @export var amount = 0
+@export var spawn_range: Vector2 = Vector2(200, 200)
 
 @onready var pickupable: PackedScene = preload("uid://dm1fmidfi4qky")
 
@@ -10,6 +11,11 @@ extends Node2D
 func _on_timer_timeout() -> void:
 	if get_child_count() - 1 < amount:
 		var item = pickupable.instantiate()
-		item.global_position = Vector2i(randi_range(80, 400), randi_range(40, 600)) 
 		item.item_data = items[randi() % items.size()]
+		var offset = Vector2(
+			randf_range(-spawn_range.x / 2, spawn_range.x / 2),
+			randf_range(-spawn_range.y / 2, spawn_range.y / 2)
+		)
+		
 		add_child(item)
+		item.global_position = global_position + offset
--- script/pickupable.gd
+++ script/pickupable.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 @export var item_data: ThrowableItem
 
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	%Sprite2D.texture = item_data.sprite
+	sprite_2d.texture = item_data.sprite
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and !body.carry:
