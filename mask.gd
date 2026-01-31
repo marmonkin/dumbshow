281aa8e95be99108cd2740d373fd6b3e118ffc1f
Vuorinen Artturi TTK23SPO <artturivuorinen@kamk.fi>
2026-01-31 15:44:29 +0200

    mask

--- mask.gd
+++ mask.gd
@@ -1,37 +1,39 @@
 extends CharacterBody2D
 
-#https://www.youtube.com/watch?v=JwxBs8OnWrI
+@onready var playernode: CharacterBody2D = get_parent().get_node("sperm guy")
 
-@export var SPEED = 150.0
-@export var MAX_ANGLE = 45.
-
-@export var player : Node2D
-var move_direction := Vector2.ZERO
-var move_timer := .3
-var wait_timer := 0.0
+var knockback: Vector2 = Vector2.ZERO
+var knockback_timer: float = 0.0
+var max_speed: float = 400.0
+var acceleration: float = 500.0
+var friction: float = 200.0
+var rotdir: float = 1
 
 func _physics_process(delta: float) -> void:
-	if not player:
-		return
-
-	if move_timer > 0.0:
-		# Move during burst
-		move_timer -= delta
-		velocity = move_direction * SPEED
-		move_and_slide()
+	rotation += .04 * rotdir;
+	if knockback_timer > 0.0:
+		velocity = knockback
+		knockback_timer -= delta
 	else:
-		# Stop and wait
-		velocity = Vector2.ZERO
-		wait_timer -= delta
+		var dir = (playernode.global_position - global_position).normalized()
+		velocity += dir * acceleration * delta
+	
+	velocity = velocity.limit_length(max_speed)
+	
+	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	move_and_slide()
 
-		if wait_timer <= 0.0:
-			start_move()
+func apply_knockback(direction: Vector2, force: float, duration: float) -> void:
+	knockback = direction * force
+	knockback_timer = duration
 
-func start_move() -> void:
-	var to_player := (player.global_position - global_position).normalized()
-
-	var angle_offset := deg_to_rad(randf_range(-MAX_ANGLE, MAX_ANGLE))
-	move_direction = to_player.rotated(angle_offset)
-
-	move_timer = randf_range(.2, .5)
-	wait_timer = randf_range(.8, 1.5)
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == playernode:
+		var dir = (global_position - body.global_position).normalized()
+		var distance = global_position.distance_to(playernode.global_position)
+		apply_knockback(dir, distance*8, .5)
+	if rotdir == 1:
+		rotdir = -1
+	else:
+		rotdir = 1
